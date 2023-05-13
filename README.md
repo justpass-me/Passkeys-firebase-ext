6d26b8420057ad343b120199e9dec984c2d8ebe0
sameh-amwal <100665288+sameh-amwal@users.noreply.github.com>
2023-05-13 12:56:09 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,89 @@
+# Justpass.me Passkey Firebase Extension 🚀
+
+## Before you begin 📝
+- Make sure you have added [Firebase to your project](https://firebase.google.com/docs/guides). If not, follow the steps in the link to get started.👍
+
+- Upgrade your project to the **Blaze** (pay as you go) plan if you haven't already. This is required for using Firebase extensions.💸
+
+- [Install or update to the latest version of the Firebase CLI.](https://firebase.google.com/docs/cli#install_the_firebase_cli) This will let you install and manage Firebase extensions from the command line.👩‍💻
+
+- Make sure Cloud Storage is Enabled for your project, You can make sure that it's enabled from this link : https://console.cloud.google.com/apis/api/firestore.googleapis.com/overview
+    - **Note** that Cloud Storage need to have atleast one database created.
+
+- Find out either your Firebase project ID or previously configured project alias. You will need this to install the extension.
+
+    - [Project ID](https://firebase.google.com/docs/projects/learn-more#project-id) — You can run this command from anywhere on your computer to see a list of your Firebase projects and their IDs.🔎
+        ```shell
+        firebase projects:list
+        ```
+    - [Project alias](https://firebase.google.com/docs/cli#project_aliases) — You can run this command from your local app directory to see which alias you have set for your project.🔖
+        ```shell
+        firebase projects:list
+        ```    
+- Go to **justpass.me** Dashboard and copy these keys. You will need them to configure the extension.🔑
+    - JUSTPASSME_ORGANIZATION_NAME
+    - JUSTPASSME_ID
+    - JUSTPASSME_API_SECRET
+
+## Install extension 🛠️
+### Development 
+
+1. Run the following command
+
+```shell
+firebase init
+```
+2. Choose `Extensions: Set up an empty Extensions manifest`
+3. run the following command
+
+```shell
+firebase ext:install . --project=projectId-or-alias
+```
+4. (Optional) if you going to update the extension code it needs to be re-built
+
+```shell
+cd /functions
+npm run build
+```
+
+### Marketplace
+
+To install the extension, run this command and replace `projectId-or-alias` with your project ID or alias.
+
+```shell
+firebase ext:install publisher-id/extension-id --project=projectId-or-alias
+```
+
+## Deploy 🚢
+To deploy the extension, run this command.
+```shell
+firebase deploy --only extensions
+```
+
+## Example Usage 📱
+Install the corresponding development SDK (iOS/Android/React Native/ Flutter) from https://www.justpass.me.
+
+Here is an example code for registration and login for react native.
+
+```Typescript
+import {register, authenticate} from 'justpass-me-react-native'
+
+const firebaseProjectName = "my-firebase-project" // the firebase project where the extension is installed
+const cloudLocation = "us-central1" // location where the extension is installed
+const extensionInstanceName = "ext-justpass-me"
+const baseURL = `https://${cloudLocation}-${firebaseProjectName}.cloudfunctions.net/${extensionInstanceName}-oidc`
+
+// registration
+const registrationURL = `${baseURL}/register/`
+const extraHeaders = {
+    Authorization: `Bearer ${await auth().currentUser.getIdToken()}`
+}
+await register(registrationURL, extraHeaders)
+
+// login
+const authenticationURL = `${baseURL}/authenticate/`
+const result = await authenticate(authenticationURL)
+if (result.token) {
+    await auth().signInWithCustomToken(result.token)
+}
+```
---
+++ functions/src/index.ts
@@ -0,0 +1,200 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import * as cors from "cors";
+import * as session from "express-session";
+import * as express from "express";
+import * as jwt from "jsonwebtoken";
+import {FirestoreStore} from "@google-cloud/connect-firestore";
+import {Firestore} from "@google-cloud/firestore";
+import {Request, Response, NextFunction} from "express";
+import {AuthorizationParameters, Issuer, generators} from "openid-client";
+
+admin.initializeApp();
+
+const app = express();
+app.use(
+  session({
+    store: new FirestoreStore({
+      dataset: new Firestore(),
+      kind: "express-sessions",
+    }),
+    secret: "my-secret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+const endpointServer = `https://${process.env.JUSTPASSME_ORGANIZATION_NAME}.accounts.justpass.me`;
+
+const justPassMeIssuer = new Issuer({
+  issuer: `${endpointServer}/openid`,
+  authorization_endpoint: `${endpointServer}/openid/authorize/`,
+  token_endpoint: `${endpointServer}/openid/token/`,
+  jwks_uri: `${endpointServer}/openid/jwks`,
+  userinfo_endpoint: `${endpointServer}/openid/userinfo/`,
+});
+
+const clientSecret = process.env.JUSTPASSME_API_SECRET!;
+
+const client = new justPassMeIssuer.Client({
+  client_id: process.env.JUSTPASSME_ID!,
+  client_secret: clientSecret,
+  response_types: ["code"],
+  id_token_signed_response_alg: "HS256",
+});
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: admin.auth.DecodedIdToken;
+    }
+  }
+}
+
+declare module "express-session" {
+  interface SessionData {
+    nonce: string;
+    state: string;
+  }
+}
+
+// Express middleware that validates Firebase ID Tokens passed in
+// the Authorization HTTP header. The Firebase ID token needs to be passed
+// as a Bearer token in the Authorization HTTP header like this:
+// `Authorization: Bearer <Firebase ID Token>`.
+// when decoded successfully, the ID Token content will be added as `req.user`.
+const authenticateFirebase = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let idToken;
+  if ( req.headers.authorization &&
+     req.headers.authorization.startsWith("Bearer ")) {
+    // Read the ID Token from the Authorization header.
+    idToken = req.headers.authorization.split("Bearer ")[1];
+  }
+
+  if (idToken) {
+    try {
+      const decodedIdToken = await admin.auth().verifyIdToken(idToken);
+      req.user = decodedIdToken;
+    } catch (error) {
+      functions.logger.error("Error while verifying Firebase ID token:", error);
+    }
+  }
+  next();
+  return;
+};
+
+const callbackURL = (req: Request): string => {
+  if (process.env.FUNCTIONS_EMULATOR) {
+    return `http://${req.get("Host")}/${process.env.GCLOUD_PROJECT}/${process.env.LOCATION}/ext-justpass-me-local-oidc`;
+  } else {
+    return `https://${req.get("Host")}/ext-justpass-me-oidc`;
+  }
+};
+
+app.use(cors());
+app.use(authenticateFirebase);
+
+const authRedirect =
+ ( req: Request, res: Response, params: AuthorizationParameters) => {
+   const nonce = generators.nonce();
+   const state = generators.state();
+   req.session.nonce = nonce;
+   req.session.state = state;
+   const finalParams = {
+     ...params,
+     scope: "openid token email profile",
+     response_mode: "code",
+     hint_mode: "token",
+     justpass_me_platform: req.get("justpass-me-platform"),
+     nonce,
+     state,
+   };
+   const signature = jwt.sign(finalParams, clientSecret);
+   const authUrl = client.authorizationUrl({
+     ...finalParams,
+     signature,
+   });
+   functions.logger.log(authUrl);
+   res.redirect(authUrl);
+ };
+
+app.get("/register", (req, res) => {
+  const {user} = req;
+  if (user) {
+    const params = {
+      login_hint: user.uid,
+      username: user.email,
+      prompt: "create",
+      redirect_uri: `${callbackURL(req)}/register_callback/`,
+    };
+    authRedirect(req, res, params);
+  } else {
+    res.status(401).json({
+      "status": "ERR",
+      "message": "Must be logged in to register passkeys",
+    });
+  }
+});
+
+app.get("/authenticate", (req, res) => {
+  const params = {
+    prompt: "login",
+    redirect_uri: `${callbackURL(req)}/authenticate_callback/`,
+  };
+  authRedirect(req, res, params);
+});
+
+app.get("/register_callback", async (req, res, next) => {
+  const params = client.callbackParams(req);
+  const {nonce, state} = req.session;
+  try {
+    const tokenSet = await client.callback(
+      `${callbackURL(req)}/register_callback/`,
+      params,
+      {nonce, state});
+    res.json({
+      "status": "OK",
+      "message": "Registered & Activated successfully, Try it on next login",
+      "claims": tokenSet.claims(),
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get("/authenticate_callback", async (req, res, next) => {
+  const params = client.callbackParams(req);
+  const {nonce, state} = req.session;
+  try {
+    const tokenSet = await client.callback(
+      `${callbackURL(req)}/authenticate_callback/`,
+      params,
+      {nonce, state});
+    if (tokenSet.access_token) {
+      const userinfo = await client.userinfo(tokenSet.access_token);
+      if (userinfo.preferred_username) {
+        const customToken = await admin.auth()
+          .createCustomToken(userinfo.preferred_username);
+        res.json({
+          "status": "OK",
+          "message": "Logged in successfully",
+          "token": customToken,
+        });
+        return;
+      }
+    }
+    res.status(401).json({
+      "status": "ERR",
+      "message": "Login failed",
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+exports.oidc = functions.https.onRequest(app);
